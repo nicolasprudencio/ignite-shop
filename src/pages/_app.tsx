6c651f5befbc4b6885674da3e1ae08fee65384be
Nicolas Prudencio <nicolas-prudencio@hotmail.com>
2022-12-05 14:31:10 -0300

    adição do Image from next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,16 @@
 import type { AppProps } from 'next/app'
+import { globalStyles } from '../styles/global'
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
+globalStyles()
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <header>
+        <Image src={logoImg.src} alt="" width={100} height={100} />
+      </header>
+      <Component {...pageProps} />
+    </div>
+  )
 }
